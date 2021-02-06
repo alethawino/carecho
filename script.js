23a96cff527066ada8b1df65d8cfe81d9fdc6b26
alethawino <omar.essa.lcc@gmail.com>
2021-02-05 22:53:23 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='index.html'>Home</a></li>" +
+"<li><a href='Inventory.html'>Inventory</a></li>" +
+"<li><a href='Contact.html'>Contact Us</a></li>" +
+"<li><a href='Signup.html'>Sign up</a></li>" +
+"<li><a href='About.html'>About</a></li>" 
+"</ul>";
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length} ;
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
